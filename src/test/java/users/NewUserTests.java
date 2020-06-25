6ee9ab90611667261b7b22742f6b79c5eafc4148
githubhair@gmail.com <C0mmun3!@>
2020-06-25 16:32:27 -0600

    Adding DeleteEmployeePage

--- src/test/java/users/NewUserTests.java
+++ src/test/java/users/NewUserTests.java
@@ -11,62 +11,24 @@ import pages.*;
 import static org.testng.Assert.assertEquals;
 
 public class NewUserTests extends BaseTests {
-    private String userId;
-    private String usernamePasswordId;
 
-    //Creates New Employee
+    //Validates Employee login
     @Test
-    public void getNewName() throws InterruptedException {
-        //Generates a guid to be used for a new name
-        NewUserPage newUserPage = new NewUserPage();
-        String guid = newUserPage.nameGenerator();
-        usernamePasswordId = guid;
+    public void ValidateEmployeeLogin() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.setUsername(randomlyGeneratedId);
+        loginPage.setPassword(randomlyGeneratedId);
+        loginPage.clickLoginButton();
 
-        //Select the PIM link
-        DashboardPage dashboard = new DashboardPage(driver);
-        dashboard.clickPIMLink();
-
-        EmployeeInformationPage employeeInformationPage = new EmployeeInformationPage(driver);
-
-        //Wait, then click Add button
-        AddEmployeePage addEmployeePage = employeeInformationPage.clickAddEmployeeButton();
-
-        //Enter the first and last name fields
-        addEmployeePage.enterFirstName(guid);
-        addEmployeePage.enterLastName(guid);
-
-
-        //String uniqueId = addEmployeePage.getEmployeeId();
-        //System.out.print(uniqueId);
-        //userId = uniqueId;
-
-        //Creates credentialed user
-        addEmployeePage.clickLoginCredentialsCheckbox();
-        addEmployeePage.createNewUsername(usernamePasswordId);
-        addEmployeePage.createNewPassword(usernamePasswordId);
-        addEmployeePage.confirmNewPassword(usernamePasswordId);
-        addEmployeePage.clickSaveButton();
-        addEmployeePage.clickWelcomeLink();
-        addEmployeePage.clickLogout();
     }
-
-    //Validates the newly created user can login
-        @Test
-        public void ValidateEmployeeLogin() throws InterruptedException {
-            LoginPage loginPage = new LoginPage(driver);
-            loginPage.setUsername(usernamePasswordId);
-            loginPage.setPassword(usernamePasswordId);
-            loginPage.clickLoginButton();
-            DashboardPage dashboard = new DashboardPage(driver);
-            assertEquals(dashboard.getHeaderText(), "Dashboard");
-
-            EmployeeInformationPage employeeInformationPage = new EmployeeInformationPage(driver);
-            AddEmployeePage addEmployeePage = employeeInformationPage.clickAddEmployeeButton();
-            addEmployeePage.clickWelcomeLink();
-            addEmployeePage.clickLogout();
-        }
-
 }
+//            DashboardPage dashboard = new DashboardPage(driver);
+//            assertEquals(dashboard.getHeaderText(), "Dashboard");
+
+//            AddEmployeePage addEmployeePage = new AddEmployeePage(driver);
+//            addEmployeePage.clickWelcomeLink();
+//            addEmployeePage.clickLogout();
+
 
 //    @AfterTest
 //    public void testCleanup() {
