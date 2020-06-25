9948227f98e3ee31aea867124247ac0581fca040
githubhair@gmail.com <C0mmun3!@>
2020-06-25 10:38:24 -0600

    Adding DeleteEmployeePage

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -2,6 +2,9 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import pages.DashboardPage;
 import pages.LoginPage;
@@ -12,7 +15,7 @@ public class BaseTests {
 
     protected WebDriver driver;
 
-    @BeforeTest
+    @BeforeClass
     public void baseSetup() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
 
@@ -21,11 +24,8 @@ public class BaseTests {
 
         driver = new ChromeDriver();
         driver.get("https://opensource-demo.orangehrmlive.com/");
-        //driver.manage().window().maximize();
+        driver.manage().window().maximize();
         //driver.manage().window().setSize(new Dimension(925, 950));
-    }
-    @BeforeTest
-    public void testLoginSuccess() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.setUsername("Admin");
         loginPage.setPassword("admin123");
@@ -34,8 +34,7 @@ public class BaseTests {
     }
 
 //    @AfterClass
-//    public void tearDown()
-//    {
+//    public void tearDown() {
 //        driver.quit();
 //    }
 
--- src/test/java/users/NewUserTests.java
+++ src/test/java/users/NewUserTests.java
@@ -2,41 +2,89 @@ package users;
 
 import base.BaseTests;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import pages.EmployeeInformationPage;
-import pages.DashboardPage;
-import pages.NewUserPage;
+import pages.*;
+
+import static org.testng.Assert.assertEquals;
 
 public class NewUserTests extends BaseTests {
+    private String userId;
+    private String usernamePasswordId;
+
+    //Creates New Employee
     @Test
-    public void getNewName() {
+    public void getNewName() throws InterruptedException {
         //Generates a guid to be used for a new name
         NewUserPage newUserPage = new NewUserPage();
-        String name = newUserPage.nameGenerator();
+        String guid = newUserPage.nameGenerator();
+        usernamePasswordId = guid;
 
         //Select the PIM link
         DashboardPage dashboard = new DashboardPage(driver);
         dashboard.clickPIMLink();
-    }
-    @Test
-    public void hoverAndClick() throws InterruptedException {
-        EmployeeInformationPage addEmployee = new EmployeeInformationPage(driver);
-        addEmployee.addUser();
-        Assert.assertEquals(addEmployee.validateAddEmployeeText(), "Add Employee");
 
+        EmployeeInformationPage employeeInformationPage = new EmployeeInformationPage(driver);
+
+        //Wait, then click Add button
+        AddEmployeePage addEmployeePage = employeeInformationPage.clickAddEmployeeButton();
+
+        //Enter the first and last name fields
+        addEmployeePage.enterFirstName(guid);
+        addEmployeePage.enterLastName(guid);
+
+
+        //String uniqueId = addEmployeePage.getEmployeeId();
+        //System.out.print(uniqueId);
+        //userId = uniqueId;
+
+        //Creates credentialed user
+        addEmployeePage.clickLoginCredentialsCheckbox();
+        addEmployeePage.createNewUsername(usernamePasswordId);
+        addEmployeePage.createNewPassword(usernamePasswordId);
+        addEmployeePage.confirmNewPassword(usernamePasswordId);
+        addEmployeePage.clickSaveButton();
+        addEmployeePage.clickWelcomeLink();
+        addEmployeePage.clickLogout();
     }
+
+    //Validates the newly created user can login
+        @Test
+        public void ValidateEmployeeLogin() throws InterruptedException {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.setUsername(usernamePasswordId);
+            loginPage.setPassword(usernamePasswordId);
+            loginPage.clickLoginButton();
+            DashboardPage dashboard = new DashboardPage(driver);
+            assertEquals(dashboard.getHeaderText(), "Dashboard");
+
+            EmployeeInformationPage employeeInformationPage = new EmployeeInformationPage(driver);
+            AddEmployeePage addEmployeePage = employeeInformationPage.clickAddEmployeeButton();
+            addEmployeePage.clickWelcomeLink();
+            addEmployeePage.clickLogout();
+        }
+
 }
-//}
 
-//        Enter First and last names.
-//        addEmployee.enterFirstName(name);
-//        addEmployee.enterLastName(name);
-
-//    }
-//    @Test
-//     public void useName() {
-//
-//
-//    }
 //    @AfterTest
-//    public void testCleanup(){
+//    public void testCleanup() {
+//        LoginPage loginPage = new LoginPage(driver);
+//        loginPage.setUsername("Admin");
+//        loginPage.setPassword("admin123");
+//        DashboardPage dashboard = loginPage.clickLoginButton();
+//        dashboard.clickPIMLink();
+//        AddEmployeePage addEmployeePage = new AddEmployeePage(driver);
+//        addEmployeePage.clickSearchButton();
+//        addEmployeePage.enterUserId(usernamePasswordId);
+//        addEmployeePage.clickIdCheckbox();
+//        DeleteEmployeePage deleteEmployeePage = new DeleteEmployeePage(driver);
+//        deleteEmployeePage.clickDeleteButton();
+//        deleteEmployeePage.clickConfirmDeleteOk();
+//
+//        dashboard.clickPIMLink();
+//        addEmployeePage.clickWelcomeLink();
+//        addEmployeePage.clickLogout();
+//    }
+//}
